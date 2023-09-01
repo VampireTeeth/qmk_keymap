cdbebd528fb7ab0b72d2a2449f565a11210fa447
Steven Liu <stevenweikeliu@canva.com>
2023-09-01 16:38:49 +1000

    Better keybinding

--- ergodox_ez/vampireteeth/keymap.c
+++ ergodox_ez/vampireteeth/keymap.c
@@ -24,29 +24,29 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [QWERTY] = LAYOUT_ergodox_pretty(
   // left hand
-  TT(MOUSE),             KC_1,                 KC_2,                    KC_3,             KC_4,                KC_5,           KC_NO,                KC_NO,          KC_6,                KC_7,                KC_8,           KC_9,            KC_0,                    KC_NO,
-  KC_TAB,                KC_Q,                 KC_W,                    KC_E,             KC_R,                KC_T,           KC_NO,                KC_NO,          KC_Y,                KC_U,                KC_I,           KC_O,            KC_P,                    KC_BSPC,
-  KC_ESC,                KC_A,                 KC_S,                    KC_D,             LT(NONNUMSYMB,KC_F), LT(ARITH,KC_G),                                       LT(ARITH,KC_H),      LT(NONNUMSYMB,KC_J), KC_K,           KC_L,            KC_SCLN,                 KC_QUOT,
+  KC_EQL,                KC_1,                 KC_2,                    KC_3,             KC_4,                KC_5,           KC_NO,                KC_NO,          KC_6,                KC_7,                KC_8,           KC_9,            KC_0,                    KC_MINS,
+  LT(ARITH,KC_TAB),      KC_Q,                 KC_W,                    KC_E,             KC_R,                KC_T,           KC_NO,                KC_NO,          KC_Y,                KC_U,                KC_I,           KC_O,            KC_P,                    LT(ARITH,KC_BSPC),
+  LT(NONNUMSYMB,KC_ESC), KC_A,                 KC_S,                    KC_D,             KC_F,                KC_G,                                                 KC_H,                KC_J,                KC_K,           KC_L,            KC_SCLN,                 LT(NONNUMSYMB,KC_QUOT),
   SFT_T(KC_BSPC),        CTL_T(KC_Z),          GUI_T(KC_X),             ALT_T(KC_C),      KC_V,                KC_B,           ALL_T(KC_NO),         MEH_T(KC_NO),   KC_N,                KC_M,                ALT_T(KC_COMM), GUI_T(KC_DOT),   CTL_T(KC_SLSH),          SFT_T(KC_ENT),
-  TT(MDIA),              KC_LCTL,              KC_LGUI,                 KC_LALT,          TG(NAVI),                                                                  KC_LEFT,             KC_DOWN,             KC_UP,          KC_RIGHT,        TT(GAME),
+  TT(MDIA),              KC_TILD,              KC_BSLS,                 KC_LBRC,          KC_RBRC,                                                                   KC_LEFT,             KC_DOWN,             KC_UP,          KC_RIGHT,        TT(GAME),
 
-                                                                                                           ALT_T(KC_APP), KC_LGUI,                   KC_LALT, CTL_T(KC_ESC),
-                                                                                                                          KC_HOME,                   KC_PGUP,
-                                                                                                 LSFT_T(KC_SPC),  KC_TAB, KC_END,                    KC_PGDN, KC_TAB, RSFT_T(KC_SPC)
+                                                                                                                 KC_LGUI, KC_LCTL,                   KC_RCTL, KC_LGUI,
+                                                                                                                          KC_LALT,                   KC_RALT,
+                                                                                                 LSFT_T(KC_SPC), KC_BSPC,TT(NAVI),                   TT(NAVI),KC_ENT, RSFT_T(KC_SPC)
 ),
 
 
 [DVORAK] = LAYOUT_ergodox_pretty(
   // left hand
-  TT(MOUSE),             KC_1,                 KC_2,                    KC_3,             KC_4,                KC_5,           KC_NO,                KC_NO,          KC_6,                KC_7,                KC_8,           KC_9,            KC_0,                    KC_NO,
-  KC_TAB,                KC_QUOT,              KC_COMM,                 KC_DOT,           KC_P,                KC_Y,           KC_NO,                KC_NO,          KC_F,                KC_G,                KC_C,           KC_R,            KC_L,                    KC_BSPC,
-  KC_ESC,                KC_A,                 KC_O,                    KC_E,             LT(NONNUMSYMB,KC_U), LT(ARITH,KC_I),                                       LT(ARITH,KC_D),      LT(NONNUMSYMB,KC_H), KC_T,           KC_N,            KC_S,                    KC_SLSH,
+  KC_EQL,                KC_1,                 KC_2,                    KC_3,             KC_4,                KC_5,           KC_NO,                KC_NO,          KC_6,                KC_7,                KC_8,           KC_9,            KC_0,                    KC_MINS,
+  KC_TAB,                KC_QUOT,              KC_COMM,                 KC_DOT,           KC_P,                KC_Y,           KC_NO,                KC_NO,          KC_F,                KC_G,                KC_C,           KC_R,            KC_L,                    LT(ARITH,KC_BSPC),
+  LT(NONNUMSYMB,KC_ESC), KC_A,                 KC_O,                    KC_E,             KC_U,                KC_I,                                                 KC_D,                KC_H,                KC_T,           KC_N,            KC_S,                    LT(NONNUMSYMB,KC_SLSH),
   SFT_T(KC_BSPC),        CTL_T(KC_SCLN),       GUI_T(KC_Q),             ALT_T(KC_J),      KC_K,                KC_X,           ALL_T(KC_NO),         MEH_T(KC_NO),   KC_B,                KC_M,                ALT_T(KC_W),    GUI_T(KC_V),     CTL_T(KC_Z),             SFT_T(KC_ENT),
-  TT(MDIA),              KC_LCTL,              KC_LGUI,                 KC_LALT,          TG(NAVI),                                                                  KC_LEFT,             KC_DOWN,             KC_UP,          KC_RIGHT,        TT(GAME),
+  TT(MDIA),              KC_TILD,              KC_BSLS,                 KC_LBRC,          KC_RBRC,                                                                   KC_LEFT,             KC_DOWN,             KC_UP,          KC_RIGHT,        TT(GAME),
 
-                                                                                                           ALT_T(KC_APP), KC_LGUI,                   KC_LALT, CTL_T(KC_ESC),
-                                                                                                                          KC_HOME,                   KC_PGUP,
-                                                                                                 LSFT_T(KC_SPC),  KC_TAB, KC_END,                    KC_PGDN, KC_TAB, RSFT_T(KC_SPC)
+                                                                                                                 KC_LGUI, KC_LCTL,                   KC_RCTL, KC_LGUI,
+                                                                                                                          KC_LALT,                   KC_RALT,
+                                                                                                 LSFT_T(KC_SPC), KC_BSPC,TT(NAVI),                   TT(NAVI),KC_ENT, RSFT_T(KC_SPC)
 ),
 
 
